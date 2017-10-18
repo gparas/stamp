aaa7e95b22b78c337fd6b8f55986612067c7b889
Georgios Parapanissios <gparapanisios@tripsta.com>
2017-10-18 12:39:32 +0300

    update user profiles

--- src/components/testimonials/HappyCustomers.js
+++ src/components/testimonials/HappyCustomers.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 
+import wellkerned from './wellkerned.jpg';
+import blumenfeld from './blumenfeld.jpg';
+import davemorin from './davemorin.jpg';
+
 const testimonials = function () {
   const items = [
     {
-      name: 'Amy Honor',
-      caption: '@a.honor',
+      name: 'Kiley Werezak',
+      avatar: wellkerned,
+      caption: '@well_kerned',
       date: '18 Aug 2016',
       text: 'Great talents are the most lovely and often the #most dangerous fruits on the tree of humanity'
     },
     {
-      name: 'Alex Douglas',
-      caption: '@a.doug',
+      name: 'Ben Blumenfeld',
+      avatar: blumenfeld,
+      caption: '@blumenfeld',
       date: '5 Jun 2016',
       text: 'It is nice having such fast and responsive support. Support is top notch'
     },
     {
-      name: 'Donald Best',
-      caption: '@bestofthebest',
+      name: 'Dave Morin',
+      avatar: davemorin,
+      caption: '@davemorin',
       date: '3 Jun 2016',
       text: '#Uiio understands the correlation between a customers needs and the work agreement'
     }
@@ -26,8 +33,15 @@ const testimonials = function () {
     <div key={i} className="grid-item width-md-1-3">
       <div className="card mb-sm">
         <div className="card-body">
-          <div className="text-name">{item.name}</div>
-          <p className="text-caption text-muted">{item.caption}</p>
+          <div className="media mb-md">
+            <div className="media-left">
+              <img className="avatar" src={item.avatar} with="48" height="48" alt={item.name}/>
+            </div>
+            <div className="media-body">
+              <div className="text-name">{item.name}</div>
+              <div className="text-caption text-muted">{item.caption}</div>
+            </div>
+          </div>
           <p>{item.text}</p>
           <div className="text-muted">{item.date}</div>
         </div>
@@ -42,7 +56,7 @@ class HappyCustomers extends Component {
       <section className="section-hero bg-accent">
         <div className="page-width">
           <h1 className="text-white text-center">Happy Customers</h1>
-          <p className="text-subheader text-white text-center mb-lg">
+          <p className="text-subheader text-white text-center mb-lg text-faded">
             Co-ordinate campaigns and product launches, with improved overall communication.
           </p>
           <div className="grid">
--- src/components/testimonials/OurClients.js
+++ src/components/testimonials/OurClients.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 
+import wellkerned from './wellkerned.jpg';
+import blumenfeld from './blumenfeld.jpg';
+import davemorin from './davemorin.jpg';
+
 const testimonials = function () {
   const items = [
     {
-      name: 'Donald Best',
-      caption: 'Head Developer',
-      text: 'Uiio has eased the client intake process and allowed me ditch that constant stream of emails that build up from clients during a project'
-    },
-    {
-      name: 'Amy Honor',
+      name: 'Ben Blumenfeld',
+      avatar: blumenfeld,
       caption: 'Senior Product Designer',
       text: 'Since switching over to Uiio, my anxiety of faulty systems has washed away and my clients have remarked multiple times'
     },
     {
-      name: 'Alex Douglas',
+      name: 'Kiley Werezak',
+      avatar: wellkerned,
+      caption: 'Head Developer',
+      text: 'Uiio has eased the client intake process and allowed me ditch that constant stream of emails that build up from clients during a project'
+    },
+    {
+      name: 'Dave Morin',
+      avatar: davemorin,
       caption: 'CTO',
       text: 'I used to spend hours creating questionnaire forms for my clients, and more than once had to explain all of their input had been lost'
     }
@@ -23,6 +30,7 @@ const testimonials = function () {
     <div key={i} className="grid-item width-md-1-3">
       <div className="card mb-sm">
         <div className="card-body">
+          <img className="avatar mb-md" src={item.avatar} with="80" height="80" alt={item.name}/>
           <p>{item.text}</p>
         </div>
         <div className="card-footer card-footer-flush">
